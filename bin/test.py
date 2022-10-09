c1aa2daf8ae4944079ae81a7a266ebfcf0eb9c66
Justus Perlwitz <mail@justusperlwitz.com>
2022-10-09 13:29:33 +0900

    Add test command

---
+++ bin/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Do a test run and see if git output changed."""
+import subprocess
+
+
+CONVERT = (
+    "./convert-statement.py",
+    "--config",
+    "test/data/config.toml",
+)
+
+GIT_STATUS = (
+    "git",
+    "diff",
+    "--exit-code",
+    "test/data/out/",
+)
+
+
+def main():
+    """Run program."""
+    subprocess.run(CONVERT, check=True)
+    subprocess.run(GIT_STATUS, check=True)
+
+
+if __name__ == "__main__":
+    main()
