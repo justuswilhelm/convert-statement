0c17b73558b859685b20fa3cf06a9b5778cb4219
Justus Perlwitz <mail@justusperlwitz.com>
2022-10-09 18:54:08 +0900

    Format the code

--- convert-statement.py
+++ convert-statement.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 """Parse DKB, Shinsei, SMBC bank statements and make them YNAB compatible."""
 import logging
-from argparse import ArgumentParser
-from csv import DictReader, DictWriter
-from datetime import datetime
-from decimal import Decimal
-from glob import glob
-from os import makedirs, path
+from csv import (
+    DictReader,
+    DictWriter,
+)
+from datetime import (
+    datetime,
+)
+from decimal import (
+    Decimal,
+)
+from glob import (
+    glob,
+)
+from os import (
+    makedirs,
+    path,
+)
 
 import toml
 
@@ -28,97 +39,95 @@ def make_ynab(rows, mapping, create_negative_rows=True):
             sub[translated_key] = value
         if create_negative_rows:
             is_negative = sub["Inflow"] < 0
-            sub['Outflow'] = abs(sub["Inflow"]) if is_negative else 0
-            sub['Inflow'] = sub["Inflow"] if not is_negative else 0
+            sub["Outflow"] = abs(sub["Inflow"]) if is_negative else 0
+            sub["Inflow"] = sub["Inflow"] if not is_negative else 0
         sub_selection.append(sub)
-    sub_selection.sort(key=lambda row: row['Date'])
+    sub_selection.sort(key=lambda row: row["Date"])
     return sub_selection
 
 
 def parse_dkb_row(row):
     """Parse dates and numerical values in DKB data."""
-    row['Wertstellung'] = datetime.strptime(
-        row['Wertstellung'],
+    row["Wertstellung"] = datetime.strptime(
+        row["Wertstellung"],
         "%d.%m.%Y",
     )
-    if 'Belegdatum' in row:
-        row['Belegdatum'] = datetime.strptime(
-            row['Belegdatum'],
+    if "Belegdatum" in row:
+        row["Belegdatum"] = datetime.strptime(
+            row["Belegdatum"],
             "%d.%m.%Y",
         )
-    if 'Buchungstag' in row:
-        row['Buchungstag'] = datetime.strptime(
-            row['Buchungstag'],
+    if "Buchungstag" in row:
+        row["Buchungstag"] = datetime.strptime(
+            row["Buchungstag"],
             "%d.%m.%Y",
         )
-    row['Betrag (EUR)'] = Decimal(
-        row['Betrag (EUR)'].replace(
-            '.', ''
-        ).replace(',', '.')
+    row["Betrag (EUR)"] = Decimal(
+        row["Betrag (EUR)"].replace(".", "").replace(",", ".")
     )
-    del row['']
+    del row[""]
     return row
 
 
 def parse_shinsei_row(row):
     """Parse numerical values in Shinsei data."""
-    if 'CR' in row:
-        row['CR'] = int(row['CR'] or 0)
-        row['DR'] = int(row['DR'] or 0)
+    if "CR" in row:
+        row["CR"] = int(row["CR"] or 0)
+        row["DR"] = int(row["DR"] or 0)
     else:
-        row['DR'] = int(row['お支払金額'] or 0)
-        row['CR'] = int(row['お預り金額'] or 0)
-        row['Value Date'] = row['取引日']
-        row['Description'] = row['摘要']
+        row["DR"] = int(row["お支払金額"] or 0)
+        row["CR"] = int(row["お預り金額"] or 0)
+        row["Value Date"] = row["取引日"]
+        row["Description"] = row["摘要"]
     return row
 
 
 def parse_new_shinsei_row(row):
     """Parse numerical values in new Shinsei data."""
     try:
-        row['DR'] = int(row['出金金額'] or 0)
-        row['CR'] = int(row['入金金額'] or 0)
-        row['Value Date'] = row['取引日']
-        row['Description'] = row['摘要']
+        row["DR"] = int(row["出金金額"] or 0)
+        row["CR"] = int(row["入金金額"] or 0)
+        row["Value Date"] = row["取引日"]
+        row["Description"] = row["摘要"]
     # English Version!
     except KeyError:
-        row['DR'] = int(row['Debit'] or 0)
-        row['CR'] = int(row['Credit'] or 0)
+        row["DR"] = int(row["Debit"] or 0)
+        row["CR"] = int(row["Credit"] or 0)
     return row
 
 
 def parse_new_shinsei_row_v2(row):
     """Parse numerical values in new Shinsei v2 data."""
     try:
-        row['DR'] = int(row['出金金額'] or 0)
-        row['CR'] = int(row['入金金額'] or 0)
-        row['Value Date'] = row['取引日']
-        row['Description'] = row['摘要']
+        row["DR"] = int(row["出金金額"] or 0)
+        row["CR"] = int(row["入金金額"] or 0)
+        row["Value Date"] = row["取引日"]
+        row["Description"] = row["摘要"]
     # English Version!
     except KeyError:
-        row['DR'] = int(row['Debit'] or 0)
-        row['CR'] = int(row['Credit'] or 0)
+        row["DR"] = int(row["Debit"] or 0)
+        row["CR"] = int(row["Credit"] or 0)
     return row
 
 
 def parse_smbc_row(row):
     """Parse numerical values in a SMBC data row."""
-    row['お引出し'] = -int(row['お引出し'] or 0)
-    row['お預入れ'] = int(row['お預入れ'] or 0)
+    row["お引出し"] = -int(row["お引出し"] or 0)
+    row["お預入れ"] = int(row["お預入れ"] or 0)
     return row
 
 
 def parse_new_smbc_row(row):
     """Parse numerical values in a SMBC data row."""
-    row['お引出し'] = int(row['お引出し'] or 0)
-    row['お預入れ'] = int(row['お預入れ'] or 0)
+    row["お引出し"] = int(row["お引出し"] or 0)
+    row["お預入れ"] = int(row["お預入れ"] or 0)
     return row
 
 
 def parse_rakuten_row(row):
     """Parse numerical values in a Rakuten data row."""
-    row['取引日'] = datetime.strptime(row['取引日'], "%Y%m%d")
-    row['入出金(円)'] = int(row['入出金(円)'])
+    row["取引日"] = datetime.strptime(row["取引日"], "%Y%m%d")
+    row["入出金(円)"] = int(row["入出金(円)"])
     return row
 
 
@@ -134,16 +143,16 @@ def read_csv(csv_path, row_fn, encoding, delimiter, skip):
 def convert_shinsei(csv_path):
     """Convert Shinsei checkings account statement."""
     fields = {
-        'Value Date': 'Date',
-        'DR': 'Outflow',
-        'CR': 'Inflow',
-        'Description': 'Memo',
+        "Value Date": "Date",
+        "DR": "Outflow",
+        "CR": "Inflow",
+        "Description": "Memo",
     }
     rows = read_csv(
         csv_path,
         parse_shinsei_row,
-        encoding='utf-16',
-        delimiter='\t',
+        encoding="utf-16",
+        delimiter="\t",
         skip=8,
     )
     return make_ynab(rows, fields, create_negative_rows=False)
@@ -152,16 +161,16 @@ def convert_shinsei(csv_path):
 def convert_shinsei_new(csv_path):
     """Convert new Shinsei checkings account statement."""
     fields = {
-        'Value Date': 'Date',
-        'DR': 'Outflow',
-        'CR': 'Inflow',
-        'Description': 'Memo',
+        "Value Date": "Date",
+        "DR": "Outflow",
+        "CR": "Inflow",
+        "Description": "Memo",
     }
     rows = read_csv(
         csv_path,
         parse_new_shinsei_row,
-        encoding='shift-jis',
-        delimiter=',',
+        encoding="shift-jis",
+        delimiter=",",
         skip=0,
     )
     return make_ynab(rows, fields, create_negative_rows=False)
@@ -170,16 +179,16 @@ def convert_shinsei_new(csv_path):
 def convert_shinsei_new_v2(csv_path):
     """Convert new Shinsei checkings account statement."""
     fields = {
-        'Value Date': 'Date',
-        'DR': 'Outflow',
-        'CR': 'Inflow',
-        'Description': 'Memo',
+        "Value Date": "Date",
+        "DR": "Outflow",
+        "CR": "Inflow",
+        "Description": "Memo",
     }
     rows = read_csv(
         csv_path,
         parse_new_shinsei_row_v2,
-        encoding='utf-8-sig',
-        delimiter=',',
+        encoding="utf-8-sig",
+        delimiter=",",
         skip=0,
     )
     return make_ynab(rows, fields, create_negative_rows=False)
@@ -188,15 +197,15 @@ def convert_shinsei_new_v2(csv_path):
 def convert_smbc(csv_path):
     """Convert SMBC checkings account statement."""
     fields = {
-        '年月日': 'Date',
-        'お引出し': 'Outflow',
-        'お預入れ': 'Inflow',
-        'お取り扱い内容': "Memo",
+        "年月日": "Date",
+        "お引出し": "Outflow",
+        "お預入れ": "Inflow",
+        "お取り扱い内容": "Memo",
     }
     rows = read_csv(
         csv_path,
         parse_smbc_row,
-        encoding='shift-jis',
+        encoding="shift-jis",
         delimiter=",",
         skip=0,
     )
@@ -206,15 +215,15 @@ def convert_smbc(csv_path):
 def convert_smbc_new(csv_path):
     """Convert SMBC checkings account statement."""
     fields = {
-        '年月日': 'Date',
-        'お引出し': 'Outflow',
-        'お預入れ': 'Inflow',
-        'お取り扱い内容': "Memo",
+        "年月日": "Date",
+        "お引出し": "Outflow",
+        "お預入れ": "Inflow",
+        "お取り扱い内容": "Memo",
     }
     rows = read_csv(
         csv_path,
         parse_new_smbc_row,
-        encoding='shift-jis',
+        encoding="shift-jis",
         delimiter=",",
         skip=0,
     )
@@ -224,14 +233,14 @@ def convert_smbc_new(csv_path):
 def convert_rakuten(csv_path):
     """Convert Rakuten checkings account statement."""
     fields = {
-        '取引日': 'Date',
-        '入出金(円)': 'Inflow',
-        '入出金先内容': 'Memo',
+        "取引日": "Date",
+        "入出金(円)": "Inflow",
+        "入出金先内容": "Memo",
     }
     rows = read_csv(
         csv_path,
         parse_rakuten_row,
-        encoding='shift-jis',
+        encoding="shift-jis",
         delimiter=",",
         skip=0,
     )
@@ -241,16 +250,16 @@ def convert_rakuten(csv_path):
 def convert_giro(csv_path):
     """Convert DKB giro account statement."""
     giro_fields = {
-        'Wertstellung': 'Date',
-        'Betrag (EUR)': 'Inflow',
-        'Verwendungszweck': 'Memo',
-        'Auftraggeber / Begünstigter': 'Payee',
+        "Wertstellung": "Date",
+        "Betrag (EUR)": "Inflow",
+        "Verwendungszweck": "Memo",
+        "Auftraggeber / Begünstigter": "Payee",
     }
     rows = read_csv(
         csv_path,
         parse_dkb_row,
-        encoding='latin_1',
-        delimiter=';',
+        encoding="latin_1",
+        delimiter=";",
         skip=6,
     )
     return make_ynab(rows, giro_fields)
@@ -259,16 +268,16 @@ def convert_giro(csv_path):
 def convert_cc(csv_path):
     """Convert DKB credit card statement."""
     cc_fields = {
-        'Belegdatum': 'Date',
-        'Betrag (EUR)': 'Inflow',
-        'Beschreibung': 'Memo',
+        "Belegdatum": "Date",
+        "Betrag (EUR)": "Inflow",
+        "Beschreibung": "Memo",
     }
     try:
         rows = read_csv(
             csv_path,
             parse_dkb_row,
-            delimiter=';',
-            encoding='latin_1',
+            delimiter=";",
+            encoding="latin_1",
             skip=6,
         )
     except KeyError:
@@ -276,13 +285,13 @@ def convert_cc(csv_path):
         rows = read_csv(
             csv_path,
             parse_dkb_row,
-            delimiter=';',
-            encoding='latin_1',
+            delimiter=";",
+            encoding="latin_1",
             skip=7,
         )
     rows = make_ynab(rows, cc_fields)
     for row in rows:
-        row['Payee'] = row['Memo']
+        row["Payee"] = row["Memo"]
     return rows
 
 
@@ -296,20 +305,20 @@ def get_output_path(file_path, in_dir, out_dir):
 
 def main(kwargs):
     """Go through files in input folder and transform CSV files."""
-    in_glob = path.join(kwargs['in_dir'], '*/*.csv')
+    in_glob = path.join(kwargs["in_dir"], "*/*.csv")
     logging.debug("Looking for files matching glob '%s'", in_glob)
 
     for csv_path in glob(in_glob, recursive=True):
         mapping = {
             # shinsei_new comes before shinsei, on purpose
-            'shinsei_new_v2': convert_shinsei_new_v2,
-            'shinsei_new': convert_shinsei_new,
-            'shinsei': convert_shinsei,
-            'dkb_cc': convert_cc,
-            'dkb_giro': convert_giro,
-            'rakuten': convert_rakuten,
-            'smbc_new': convert_smbc_new,
-            'smbc': convert_smbc,
+            "shinsei_new_v2": convert_shinsei_new_v2,
+            "shinsei_new": convert_shinsei_new,
+            "shinsei": convert_shinsei,
+            "dkb_cc": convert_cc,
+            "dkb_giro": convert_giro,
+            "rakuten": convert_rakuten,
+            "smbc_new": convert_smbc_new,
+            "smbc": convert_smbc,
         }
         for k, fn in mapping.items():
             if k in csv_path:
@@ -321,12 +330,12 @@ def main(kwargs):
 
         out_path = get_output_path(
             csv_path,
-            kwargs['in_dir'],
-            kwargs['out_dir'],
+            kwargs["in_dir"],
+            kwargs["out_dir"],
         )
         makedirs(path.dirname(out_path), exist_ok=True)
         logging.info("Writing results to '%s'", out_path)
-        with open(out_path, 'w') as fd:
+        with open(out_path, "w") as fd:
             writer = DictWriter(fd, fieldnames=output[0].keys())
             writer.writeheader()
             for row in output:
