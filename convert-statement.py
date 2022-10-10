0fa986c1debbf5325d047c20c70b548986a9a1bc
Justus Perlwitz <mail@justusperlwitz.com>
2022-10-10 21:43:59 +0900

    Refactor Rakuten parsers

--- convert-statement.py
+++ convert-statement.py
@@ -31,19 +31,13 @@ from parser.dkb import (
     convert_giro,
 )
 from parser.format import (
-    CellParser,
-    ConstantParser,
     CsvFormat,
     CsvRow,
-    CsvTransactionParser,
-    ExtractDateParser,
-    ExtractParser,
     apply_date_parser,
     apply_parser,
 )
-from parser.helper import (
-    at_least_0,
-    derive_withdrawal,
+from parser.rakuten import (
+    convert_rakuten,
 )
 from parser.shinsei import (
     convert_shinsei,
@@ -96,29 +90,6 @@ TransactionDict = TypedDict(
 
 
 fieldnames: List[str] = [f.name for f in fields(Transaction)]
-
-
-# Rakuten
-def 入出金(row: CsvRow) -> Decimal:
-    """Extract withdrawal / deposit from Rakuten row."""
-    return Decimal(row["入出金(円)"])
-
-
-rakuten_parser = CsvTransactionParser(
-    date=ExtractDateParser("取引日", "%Y%m%d"),
-    withdrawal=CellParser(lambda row: derive_withdrawal(入出金(row))),
-    deposit=CellParser(lambda row: at_least_0(入出金(row))),
-    description=ExtractParser("入出金先内容", str),
-    memo=ConstantParser(""),
-    num=ConstantParser(""),
-)
-convert_rakuten = CsvFormat(
-    parser=rakuten_parser,
-    encoding="shift-jis",
-    delimiter=",",
-    skip=0,
-    path="rakuten",
-)
 
 
 formats: Iterable[CsvFormat] = (
---
+++ parser/rakuten.py
@@ -0,0 +1,39 @@
+"""Contain the SMBC parsers."""
+from decimal import (
+    Decimal,
+)
+from parser.format import (
+    CellParser,
+    ConstantParser,
+    CsvFormat,
+    CsvRow,
+    CsvTransactionParser,
+    ExtractDateParser,
+    ExtractParser,
+)
+from parser.helper import (
+    at_least_0,
+    derive_withdrawal,
+)
+
+
+def 入出金(row: CsvRow) -> Decimal:
+    """Extract withdrawal / deposit from Rakuten row."""
+    return Decimal(row["入出金(円)"])
+
+
+rakuten_parser = CsvTransactionParser(
+    date=ExtractDateParser("取引日", "%Y%m%d"),
+    withdrawal=CellParser(lambda row: derive_withdrawal(入出金(row))),
+    deposit=CellParser(lambda row: at_least_0(入出金(row))),
+    description=ExtractParser("入出金先内容", str),
+    memo=ConstantParser(""),
+    num=ConstantParser(""),
+)
+convert_rakuten = CsvFormat(
+    parser=rakuten_parser,
+    encoding="shift-jis",
+    delimiter=",",
+    skip=0,
+    path="rakuten",
+)
