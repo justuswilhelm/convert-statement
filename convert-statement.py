d6bfe0461d0e175f9106e88c1320bab7dfbc27aa
Justus Perlwitz <mail@justusperlwitz.com>
2022-10-10 21:41:32 +0900

    Refactor SMBC parsers

--- convert-statement.py
+++ convert-statement.py
@@ -52,6 +52,10 @@ from parser.shinsei import (
     convert_shinsei_new_en,
     convert_shinsei_new_v2,
 )
+from parser.smbc import (
+    convert_smbc,
+    convert_smbc_new,
+)
 from typing import (
     Iterable,
     List,
@@ -92,39 +96,6 @@ TransactionDict = TypedDict(
 
 
 fieldnames: List[str] = [f.name for f in fields(Transaction)]
-
-
-# SMBC
-smbc_parser = CsvTransactionParser(
-    date=ExtractDateParser("年月日", "%Y/%m/%d"),
-    withdrawal=CellParser(lambda row: -Decimal(row["お引出し"] or 0)),
-    deposit=CellParser(lambda row: Decimal(row["お預入れ"] or 0)),
-    description=ExtractParser("お取り扱い内容", str),
-    memo=ConstantParser(""),
-    num=ConstantParser(""),
-)
-smbc_new_parser = CsvTransactionParser(
-    date=ExtractDateParser("年月日", "%Y/%m/%d"),
-    withdrawal=CellParser(lambda row: Decimal(row["お引出し"] or 0)),
-    deposit=CellParser(lambda row: Decimal(row["お預入れ"] or 0)),
-    description=ExtractParser("お取り扱い内容", str),
-    memo=ConstantParser(""),
-    num=ConstantParser(""),
-)
-convert_smbc = CsvFormat(
-    parser=smbc_parser,
-    encoding="shift-jis",
-    delimiter=",",
-    skip=0,
-    path="smbc",
-)
-convert_smbc_new = CsvFormat(
-    parser=smbc_new_parser,
-    encoding="shift-jis",
-    delimiter=",",
-    skip=0,
-    path="smbc_new",
-)
 
 
 # Rakuten
